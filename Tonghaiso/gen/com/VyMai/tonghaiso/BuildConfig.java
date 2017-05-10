3cc6fd2bcc1a5f0be52e724c024f85589dc5f8db
Vy Mai <Vy Mai>
2017-05-10 13:44:10 +0700

    them

---
+++ Tonghaiso/gen/com/VyMai/tonghaiso/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.VyMai.tonghaiso;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ VIDU_Hello/src/com/VyMai/vidu_hello/MainActivity.java
@@ -0,0 +1,27 @@
+package com.VyMai.vidu_hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	TextView txt01; //ctrl+shift+o
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt01 = (TextView)findViewById(R.id.textView01);
+        txt01.setText("hello word ");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
